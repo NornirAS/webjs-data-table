4cfa3c4bc3f896ce3bd7744a76dae07a850d9a13
Leon2ev <jevgeni.glazunov@gmail.com>
2021-08-05 18:05:48 +0200

    Added chat.

---
+++ chat.js
@@ -0,0 +1,23 @@
+const list = document.createElement("ul");
+list.style.listStyleType = "none";
+list.style.overflow = "scroll";
+list.style.position = "relative";
+list.style.margin = "0";
+list.style.padding = "0.5em 0";
+
+const appElement = document.getElementById("webjs-data-table");
+appElement.appendChild(list);
+
+const li = document.createElement("li");
+li.innerHTML = "Chat loaded..."
+list.append(li)
+this.gotWebJS = true;
+
+const displayData = ({ RTW }) => {
+  const { TIME, USERNAME, UPPERCASE_MSG } = RTW;
+  const msg = `${TIME} ${USERNAME}: ${UPPERCASE_MSG}`;
+  const list = document.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerHTML = msg;
+  list.append(li);
+};
